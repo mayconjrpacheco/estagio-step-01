c914fe0dcd732625df53e6369bd7546cdb7b8476
MarkusNt <markus_nathan@live.com>
2018-08-24 19:01:36 -0300

    Profile Interface & Js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Example of a random customer generator
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
