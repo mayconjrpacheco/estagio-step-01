14e0c4e594df357e83956ae4f810a059d1caa953
danillo <danillopkt@gmail.com>
2018-08-24 16:41:40 -0300

    update

--- estagio/_js/main.js
+++ estagio/_js/main.js
@@ -1,7 +1,6 @@
 var query = location.search.slice(1);
-if(!query){
-    getUserData();
-}
+
+getUserData();
 
 
 function getUserData() {
@@ -17,42 +16,35 @@ function getUserData() {
 }
 
 
-function renderUserListItem(listUserData) {
-    console.log(listUserData);
-}
+    function renderUserList(listUserData) {
 
+        console.log(listUserData);
 
-function renderUserList(listUserData) {
-    recordUser(listUserData[0]);
-    console.log(listUserData[0])
-    renderUserListItem(listUserData);
-    $('<ul>', {
-        id: 'list'
-    }).appendTo('#clients-list');
-    var templateItem = '';
-    for (var i = 0; i < listUserData.length; i++) {
-        
-        templateItem += '<div class="clients-menu">';
-        templateItem += '<div class="clients-box">';
-        templateItem += '<div class="container">';
-        templateItem += '<div class="item basis-auto"><img src='+ listUserData[i].picture.thumbnail+'></div>';
-        templateItem += '<div class="item basis-auto" id="name"><a href="profile.html?'+ i +'">'+ listUserData[i].name.first +'</a></div>';
-        templateItem += '<div class="item basis-auto">'+ listUserData[i].email +'</div>';
-        templateItem += '<div class="item basis-auto">'+ listUserData[i].phone +'</div>';
-        templateItem += '<div class="item basis-auto" id="local">'+ listUserData[i].location.city +' - '+ listUserData[i].location.state + '</div>';
-        templateItem += '<div class="item basis-auto ic">'
-        templateItem += '<i class="fas fa-trash icons"></i>'
-        templateItem += '<i class="fas fa-check icons"></i>'
-        templateItem += '<i class="fas fa-th-list icons"></i>'
-        templateItem += '</div></div></div></div>';
-        $("<li>" + templateItem + "</li>").appendTo('#list');
+        $('<ul>', {
+            id: 'list'
+        }).appendTo('#clients-list');
+        var templateItem = '';
+        for (var i = 0; i < listUserData.length; i++) {
+            
+            templateItem += '<div class="clients-menu">';
+            templateItem += '<div class="clients-box">';
+            templateItem += '<div class="container">';
+            templateItem += '<div class="item basis-auto"><img src='+ listUserData[i].picture.thumbnail+'></div>';
+            templateItem += '<div class="item basis-auto" id="name"><a href="profile.html? '+ listUserData[i].name.first +' ">'+ listUserData[i].name.first +'</a></div>';
+            templateItem += '<div class="item basis-auto">'+ listUserData[i].email +'</div>';
+            templateItem += '<div class="item basis-auto">'+ listUserData[i].phone +'</div>';
+            templateItem += '<div class="item basis-auto" id="local">'+ listUserData[i].location.city +' - '+ listUserData[i].location.state + '</div>';
+            templateItem += '<div class="item basis-auto ic">'
+            templateItem += '<i class="fas fa-trash icons"></i>'
+            templateItem += '<i class="fas fa-check icons"></i>'
+            templateItem += '<i class="fas fa-th-list icons"></i>'
+            templateItem += '</div></div></div></div>';
+            $("<li>" + templateItem + "</li>").appendTo('#list');
+        }
     }
-}
 
 
 
 
-function recordUser(user){
-    
-}
+
 
--- estagio/_js/profile.js
+++ estagio/_js/profile.js
@@ -1,3 +1,44 @@
-var query = location.search.slice(1);
+getUserData();
 
-console.log(query)
+
+function getUserData() {
+    
+    $.ajax({
+        url: 'https://randomuser.me/api/?nat=br&seed=olvani',
+        dataType: 'json',
+        success: function(data) {
+            renderUserList(data.results);
+        }
+    });
+    
+}
+
+function renderUserListItem(listUserData) {
+    console.log(listUserData);
+}
+
+function renderUserList(listUserData) {
+    
+    renderUserListItem(listUserData);
+    $('<ul>', {
+        id: 'list'
+    }).appendTo('#clients-list');
+    var templateItem = '';
+    for (var i = 0; i < listUserData.length; i++) {
+        
+        templateItem += '<div class="clients-menu">';
+        templateItem += '<div class="clients-box">';
+        templateItem += '<div class="container">';
+        templateItem += '<div class="item basis-auto"><img src='+ listUserData[i].picture.thumbnail+'></div>';
+        templateItem += '<div class="item basis-auto" id="name"><a href="profile.html">'+ listUserData[i].name.first +'</a></div>';
+        templateItem += '<div class="item basis-auto">'+ listUserData[i].email +'</div>';
+        templateItem += '<div class="item basis-auto">'+ listUserData[i].phone +'</div>';
+        templateItem += '<div class="item basis-auto" id="local">'+ listUserData[i].location.city +' - '+ listUserData[i].location.state + '</div>';
+        templateItem += '<div class="item basis-auto ic">'
+        templateItem += '<i class="fas fa-trash icons"></i>'
+        templateItem += '<i class="fas fa-check icons"></i>'
+        templateItem += '<i class="fas fa-th-list icons"></i>'
+        templateItem += '</div></div></div></div>';
+        $("<li>" + templateItem + "</li>").appendTo('#list');
+    }
+}
